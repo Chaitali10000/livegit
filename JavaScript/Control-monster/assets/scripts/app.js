8b61250dfc481ebb38bd49c114d228be18eee80a
Chaitali10000 <chaitali.rewatkar@appxbuild.com>
2021-01-23 19:45:05 +0530

    completed break statement

--- JavaScript/Control-monster/assets/scripts/app.js
+++ JavaScript/Control-monster/assets/scripts/app.js
@@ -15,6 +15,7 @@ const enteredValue = prompt('Maximum life for you and the monster.', '100');
 
 let chosenMaxLife = parseInt(enteredValue);
 let battleLog = [];
+let lastLoggedEntry;
 
 if(isNaN(chosenMaxLife) || chosenMaxLife <= 0) {
     chosenMaxLife = 100;
@@ -34,45 +35,87 @@ function writeToLog(ev, val, monsterHealth, playerHealth) {
             finalMonsterHealth: monsterHealth,
             finalPlayerHealth: playerHealth
           };
+    switch (ev){
+     case LOG_EVENT_PLAYER_ATTACK:
+        logEntry.target = 'MONSTER';
+        break;
+        case LOG_EVENT_PLAYER_STRONG_ATTACK:
+            logEntry = {
+                event: ev,
+                value: val,
+                target: 'MONSTER',
+                finalMonsterHealth: monsterHealth,
+                finalPlayerHealth: playerHealth
+              };
+              break;
+        case LOG_EVENT_MONSTER_ATTACK:
+            logEntry = {
+                event: ev,
+                value: val,
+                target: 'PLAYER',
+                finalMonsterHealth: monsterHealth,
+                finalPlayerHealth: playerHealth
+              };
+              break;
+        case LOG_EVENT_PLAYER_HEAL:
+            logEntry = {
+                event: ev,
+                value: val,
+                target: 'PLAYER',
+                finalMonsterHealth: monsterHealth,
+                finalPlayerHealth: playerHealth
+              };
+              break;
+        case LOG_EVENT_GAME_OVER:
+            logEntry = {
+                event: ev,
+                value: val,
+                finalMonsterHealth: monsterHealth,
+                finalPlayerHealth: playerHealth
+              };
+              break;
+              default:
+                  logEntry = {};
+        }
 
-   if(ev === LOG_EVENT_PLAYER_ATTACK) {
-       logEntry.target = 'MONSTER';
+//    if(ev === LOG_EVENT_PLAYER_ATTACK) {
+//        logEntry.target = 'MONSTER';
      
-   } else if (ev === LOG_EVENT_PLAYER_STRONG_ATTACK) {
-    logEntry = {
-        event: ev,
-        value: val,
-        target: 'MONSTER',
-        finalMonsterHealth: monsterHealth,
-        finalPlayerHealth: playerHealth
-      };
+//    } else if (ev === LOG_EVENT_PLAYER_STRONG_ATTACK) {
+//     logEntry = {
+//         event: ev,
+//         value: val,
+//         target: 'MONSTER',
+//         finalMonsterHealth: monsterHealth,
+//         finalPlayerHealth: playerHealth
+//       };
      
-   } else if (ev === LOG_EVENT_MONSTER_ATTACK) {
-    logEntry = {
-        event: ev,
-        value: val,
-        target: 'PLAYER',
-        finalMonsterHealth: monsterHealth,
-        finalPlayerHealth: playerHealth
-      };
+//    } else if (ev === LOG_EVENT_MONSTER_ATTACK) {
+//     logEntry = {
+//         event: ev,
+//         value: val,
+//         target: 'PLAYER',
+//         finalMonsterHealth: monsterHealth,
+//         finalPlayerHealth: playerHealth
+//       };
      
-   } else if (ev === LOG_EVENT_PLAYER_HEAL) {
-    logEntry = {
-        event: ev,
-        value: val,
-        target: 'PLAYER',
-        finalMonsterHealth: monsterHealth,
-        finalPlayerHealth: playerHealth
-      };
+//    } else if (ev === LOG_EVENT_PLAYER_HEAL) {
+//     logEntry = {
+//         event: ev,
+//         value: val,
+//         target: 'PLAYER',
+//         finalMonsterHealth: monsterHealth,
+//         finalPlayerHealth: playerHealth
+//       };
      
-   }else if (ev === LOG_EVENT_GAME_OVER) {
-    logEntry = {
-        event: ev,
-        value: val,
-        finalMonsterHealth: monsterHealth,
-        finalPlayerHealth: playerHealth
-      };
-   }
+//    }else if (ev === LOG_EVENT_GAME_OVER) {
+//     logEntry = {
+//         event: ev,
+//         value: val,
+//         finalMonsterHealth: monsterHealth,
+//         finalPlayerHealth: playerHealth
+//       };
+//    }
    battleLog.push(logEntry);
 }
 
@@ -184,7 +227,37 @@ function healPlayerHandler() {
 }
 
 function printLogHandler() {
-    console.log(battleLog);
+    
+    for (let i = 0; i < 4; i++){
+        console.log('-----');
+    }
+    let j = 0;
+    do{
+        console.log('j');
+        j++;
+    }
+    while (j < 4);
+    // for (i = 10; i > 0; i--){
+        
+    //     console.log('i');
+    // }
+    // for (i = 0; i < battleLog.length; i++){
+    //     console.log(battleLog[i]);
+    // }
+    let i = 0;
+    for(const logEntry of battleLog){
+        if (!lastLoggedEntry && lastLoggedEntry !== 0 || lastLoggedEntry < i){
+        console.log(`#${i}`);
+        for (const key in logEntry){
+            console.log (key);
+            console.log(`${key} => ${logEntry[key]}`);
+        }
+        lastLoggedEntry = i;
+    }
+        i++;
+        break;
+    }
+   
 }
 
 attackBtn.addEventListener('click', attackHandler);
